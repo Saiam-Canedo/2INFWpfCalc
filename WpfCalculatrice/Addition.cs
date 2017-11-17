d4cb271c73cf43fbcb29edea090f751088df834f
Cañedo Saïam <CanedoSa@s2.rpn.ch>
2017-11-17 09:49:30 +0100

    Creat initial commit

---
+++ WpfCalculatrice/Addition.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCalculatrice
+{
+    /// <summary>
+    /// Simple addition class
+    /// </summary>
+    public class Addition
+    {
+        private double val1;
+        private double val2;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="v1">First Value</param>
+        /// <param name="v2">Second value</param>
+        public Addition(double v1,double v2)
+        {
+            this.val1 = v1;
+            this.val2 = v2;
+        }
+        /// <summary>
+        /// Add the two setted values
+        /// </summary>
+        /// <returns>result of sum</returns>
+        public double Addtionne()
+        {
+            return val1 + val2;
+        }
+        /// <summary>
+        /// set first value
+        /// </summary>
+        /// <param name="val">value</param>
+        public void setVal1(double val)
+        {
+            this.val1 = val;
+        }
+        /// <summary>
+        /// get first value
+        /// </summary>
+        /// <returns>first value</returns>
+        public double getVal1()
+        {
+            return this.val1;
+        }
+        /// <summary>
+        /// set second value
+        /// </summary>
+        /// <param name="val">value</param>
+        public void setVal2(double val)
+        {
+            this.val2 = val;
+        }
+        /// <summary>
+        /// get second value
+        /// </summary>
+        /// <returns>second value</returns>
+        public double getVal2()
+        {
+            return this.val2;
+        }
+
+        public string Affiche()
+        {
+            return "The sum of " + val1.ToString() 
+                + " and " + val2.ToString() 
+                + " is " + (Addtionne()).ToString();
+        }
+    }
+}
---
+++ WpfCalculatrice/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCalculatrice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Addition addition;
+        public MainWindow()
+        {
+            InitializeComponent();
+            addition = new Addition(0,0);
+        }
+
+        private void tbnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            addition.setVal1(33);
+            addition.setVal2(44);
+            lblResulta.Content = addition.Addtionne().ToString();
+            MessageBox.Show(addition.Affiche());
+        }
+
+        private void tbnSub_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void tbnMult_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tbnDiv_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
